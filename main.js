b9fde30bfc41a755e6bbd14b1a424764b9255a4c
Cheuk Yin Ng <chucksys88@gmail.com>
2020-03-11 11:50:29 -0700

    Add dragging and scaling support flawlessly might i add

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const WIDTH = 1280;
+const HEIGHT = 720;
+
 var Engine, World, Bodies;
 
 var delta = 0;
@@ -10,8 +13,15 @@ var view = {
 	x: 0, y: 0, s: 1
 }
 
+var dragging = {
+	x: 0, y: 0, inState: false,
+	vx: 0, vy: 0
+};
+
+var selected = null;
+
 function setup() {
-	createCanvas(1280, 720);
+	createCanvas(WIDTH, HEIGHT);
 
 	Engine = Matter.Engine;
 	World = Matter.World;
@@ -24,19 +34,51 @@ function setup() {
 function draw() {
 	background('black');
 
+	push();
+	// Place (0, 0) at the center of the screen
+	translate(WIDTH / 2, HEIGHT / 2);
 	for (let b of m_bodies) {
 		b.draw(view);
 	}
+	pop();
 
 	tick(deltaTime);
 }
 
 function mouseReleased() {
-	m_bodies.push(new Node(m_engine.world, mouseX, mouseY));
+	if (dragging.inState) {
+		dragging.inState = false;
+	} else {
+		let mX = (mouseX - WIDTH / 2) / view.s - view.x;
+		let mY = (mouseY - HEIGHT / 2) / view.s - view.y;
+		let n = new Node(m_engine.world, mX, mY);
+		if (selected !== null) {
+			selected.deselect();
+		}
+		selected = n;
+		m_bodies.push(n);
+	}
+}
+
+function mouseDragged() {
+	if (!dragging.inState) {
+		dragging.inState = true;
+		dragging.x = mouseX;
+		dragging.y = mouseY;
+		dragging.vx = view.x;
+		dragging.vy = view.y;
+	} else {
+		let mX = (mouseX - dragging.x) / view.s;
+		let mY = (mouseY - dragging.y) / view.s;
+		view.x = dragging.vx + mX;
+		view.y = dragging.vy + mY;
+	}
 }
 
 function mouseWheel(evt) {
 	view.s += evt.delta / 100;
+
+	return false;
 }
 
 function tick(delta) {
--- node.js
+++ node.js
@@ -1,9 +1,9 @@
-const NODE_RADIUS = 20;
+const NODE_RADIUS = 50;
 
 class Node {
 	constructor(world, x, y) {
 		this.m_body = Bodies.circle(x, y, NODE_RADIUS);
-		this.m_selected = false;
+		this.m_selected = true;
 		this.m_data = {};
 
 		World.add(world, this.m_body);
@@ -21,13 +21,25 @@ class Node {
 		if (this.m_selected) {
 			stroke('green');
 		} else {
-			stroke(50);
+			stroke('white');
 		}
 
 		push();
 		scale(v.s);
 		translate(pos.x + v.x, pos.y + v.y);
 		circle(0, 0, NODE_RADIUS);
+
+		if (this.m_data.name) {
+			fill('white');
+			noStroke();
+			textAlign(CENTER, CENTER);
+			text(this.m_data.name[0].toUpperCase(), 0, 0);
+		}
+
 		pop();
+	}
+
+	deselect() {
+		this.m_selected = false;
 	}
 }
