dd5d9710379fc9b11fd94fb5696a9c888eecb667
Cheuk Yin Ng <chucksys88@gmail.com>
2020-03-10 21:04:41 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+var Engine, World, Bodies;
+
+var delta = 0;
+var running = true;
+
+var m_engine;
+var m_bodies = [];
+
+var view = {
+	x: 0, y: 0, s: 1
+}
+
+function setup() {
+	createCanvas(1280, 720);
+
+	Engine = Matter.Engine;
+	World = Matter.World;
+	Bodies = Matter.Bodies;
+
+	m_engine = Engine.create();
+	m_engine.world.gravity.y = 0;
+}
+
+function draw() {
+	background('black');
+
+	for (let b of m_bodies) {
+		b.draw(view);
+	}
+
+	tick(deltaTime);
+}
+
+function mouseReleased() {
+	m_bodies.push(new Node(m_engine.world, mouseX, mouseY));
+}
+
+function tick(delta) {
+	Engine.update(m_engine, delta);
+}
---
+++ node.js
@@ -0,0 +1,32 @@
+const NODE_RADIUS = 20;
+
+class Node {
+	constructor(world, x, y) {
+		this.m_body = Bodies.circle(x, y, NODE_RADIUS);
+		this.m_selected = false;
+
+		World.add(world, this.m_body);
+	}
+
+	/**
+	 * Draw the node onto the screen.
+	 * @param v The current viewing area (with offsets and scale)
+	 */
+	draw(v) {
+		const pos = this.m_body.position;
+
+		fill(20);
+		strokeWeight(4);
+		if (this.m_selected) {
+			stroke('green');
+		} else {
+			stroke(50);
+		}
+
+		push();
+		scale(v.s);
+		translate(pos.x + v.x, pos.y + v.y);
+		circle(0, 0, NODE_RADIUS);
+		pop();
+	}
+}
